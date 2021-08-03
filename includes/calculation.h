f96f05475aa165323af76612e77ba6135fc14f76
junxian428 <holeyiho@gmail.com>
2021-08-04 04:02:32 +0800

    CPlus Header

---
+++ includes/calculation.h
@@ -0,0 +1,6 @@
+#include<iostream>
+
+
+int add(int, int);
+int sub(int, int);
+void output_message(std::string);
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "includes/calculation.h"
+
+int main(){
+    std::cout<<add(1,3)<<std::endl;
+    return 0;
+}
